d84bb37918635888775e168e134ab7024227b3cc
Mwandoe-Shali <mwandoeshali@gmail.com>
2023-11-12 18:08:47 +0300

    Code modifications to Claude's recommendations

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,10 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         if kwargs:
-            for key in kwargs.items():
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = kwargs[key]
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
@@ -23,14 +18,15 @@ class BaseModel():
         
 
     def __str__(self):
+        # type(self).__name__ == self.__class__.__name__
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
     
     def save(self):
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        kamusi = self.__dict__.copy()
-        kamusi["__class__"] = type(self).__name__
-        kamusi["created_at"] = kamusi["created_at"].isoformat()
-        kamusi["updated_at"] = kamusi["updated_at"].isoformat()
-        return kamusi
+        cp_dct = dict(self.__dict__)
+        cp_dct["__class__"] = type(self).__name__
+        cp_dct["created_at"] = self.created_at.isoformat()
+        cp_dct["updated_at"] = self.updated_at.isoformat()
+        return cp_dct
