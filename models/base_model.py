731ff5d981c0b06f68f7ad44b15f0a052109fa19
Mwandoe-Shali <mwandoeshali@gmail.com>
2023-11-13 01:11:30 +0300

    accomodate uuid4 import

--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,7 @@ class BaseModel():
                     setattr(self, key, type(self))
                 setattr(self, key, value)
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
             storage.new(self)
