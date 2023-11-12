26efabfbcb04c2b11fde513e455e565fcee24b22
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-11-13 02:26:49 +0300

    resolving some merge conflicts

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-
 '''
 BaseModel class definition module
 '''
@@ -68,64 +66,3 @@ class BaseModel:
         myDict['updated_at'] = myDict['updated_at'].isoformat()
         myDict['created_at'] = myDict['created_at'].isoformat()
         return myDict
-=======
-"""
-Base model module.
-Contains the BaseModel class that defines all common attributes/methods
-                        for other classes.
-"""
-
-from uuid import uuid4
-from datetime import datetime
-from models import storage
-
-class BaseModel():
-    """BaseModel class to inherit from.
-    Initializes public instance attributes:
-    - id: string - assign with an uuid when an instance is created
-    - created_at: datetime - assign with the current datetime when an instance is created
-    - updated_at: datetime - assign with the current datetime when an instance is created and
-                                                it will be updated every time you change your object
-    """
-    def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
-        Args:
-            args (any): Unused.
-            kwargs (dict): Key/value pairs of attributes.
-        """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "__class__":
-                    setattr(self, key, type(self))
-                setattr(self, key, value)
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
-            storage.new(self)
-
-    def __str__(self):
-        """Return string representation.
-        Shows class name, id, and dictionary representation.
-        """
-        # type(self).__name__ == self.__class__.__name__
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
-    
-    def save(self):
-        """Update updated_at with current time"""
-        self.updated_at = datetime.datetime.now()
-        storage.save()
-
-    def to_dict(self):
-        """Return the dictionary representation of the BaseModel.
-        Includes ISO formatted datetime strings.
-        """
-        # self.created_at.isoformat() == dict_cpy["created_at"].isoformat()
-        dict_cpy = dict(self.__dict__)
-        dict_cpy["__class__"] = type(self).__name__
-        dict_cpy["created_at"] = self.created_at.isoformat()
-        dict_cpy["updated_at"] = self.updated_at.isoformat()
-        return dict_cpy
->>>>>>> c447bbc3bfc2d4f3bd5ebb750d8d98b2fa802cd4
