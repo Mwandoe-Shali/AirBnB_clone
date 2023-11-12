dd6e4d50a773840b84cad92146b5a143034630d3
Mwandoe-Shali <mwandoeshali@gmail.com>
2023-11-13 00:02:43 +0300

    updated the hierachy of the file organisation

--- hierachy.lua
+++ hierachy.lua
@@ -1,6 +1,15 @@
 AirBnB_clone/
 │-- models/
+│   │-- engine/
+│   │   │-- __init__.py
+│   │   │-- file_storage.py
 │   │-- __init__.py
 │   │-- base_model.py
+│   │-- user.py
+│   │-- amenity.py
+│   │-- city.py
+│   │-- place.py
+│   │-- review.py
+│   │-- state.py
 │-- tests/
 │   │-- test_base_model.py
