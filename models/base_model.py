16afd5e33f0cc61ffe77e942401a4b5b159408ed
Mwandoe-Shali <mwandoeshali@gmail.com>
2023-11-12 23:50:56 +0300

    updated for storage.

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,7 @@ Contains the BaseModel class that defines all common attributes/methods
 
 import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel():
     """BaseModel class to inherit from.
@@ -33,7 +34,8 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
-        
+            storage.new(self)
+
     def __str__(self):
         """Return string representation.
         Shows class name, id, and dictionary representation.
@@ -44,6 +46,7 @@ class BaseModel():
     def save(self):
         """Update updated_at with current time"""
         self.updated_at = datetime.datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Return the dictionary representation of the BaseModel.
